e1dc679b8d68df4a772e78589353cfe6e4f6b827
Raul Astete <raulastete.2215@gmail.com>
2024-12-17 20:59:56 -0500

    Change UI files (color, theme, typography)

--- app/src/main/java/com/raulastete/kambio/ui/theme/Color.kt
+++ app/src/main/java/com/raulastete/kambio/ui/theme/Color.kt
@@ -2,10 +2,15 @@ package com.raulastete.kambio.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val KambioPrimary = Color(0xFF4A7C59)
+val KambioPrimary30 = Color(0x4D4A7C59)
+val KambioPrimary10 = Color(0x1A4A7C59)
+val KambioPrimary5 = Color(0x0D4A7C59)
+val KambioBlack = Color(0xFF080707)
+val KambioGray = Color(0xFF87938C)
+val KambioGray40 = Color(0x6687938C)
+val KambioDarkGray = Color(0xFF232624)
+val KambioWhite = Color(0xFFFAFAFA)
+val KambioWhiteVariant = Color(0xFFFAF9F6)
+val KambioDarkRed = Color(0xFFBB3D3D)
+val KambioDarkRed5 = Color(0x0DBB3D3D)
--- app/src/main/java/com/raulastete/kambio/ui/theme/Type.kt
+++ app/src/main/java/com/raulastete/kambio/ui/theme/Type.kt
@@ -2,33 +2,125 @@ package com.raulastete.kambio.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.raulastete.kambio.R
+
+val Poppins = FontFamily(
+    Font(
+        resId = R.font.poppins_light,
+        weight = FontWeight.Light
+    ),
+    Font(
+        resId = R.font.poppins_regular,
+        weight = FontWeight.Normal
+    ),
+    Font(
+        resId = R.font.poppins_medium,
+        weight = FontWeight.Medium
+    ),
+    Font(
+        resId = R.font.poppins_semibold,
+        weight = FontWeight.SemiBold
+    ),
+    Font(
+        resId = R.font.poppins_bold,
+        weight = FontWeight.Bold
+    ),
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
-    bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+    labelSmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Light,
+        fontSize = 12.sp,
+        lineHeight = 20.sp,
+        color = KambioWhiteVariant
+    ),
+    labelMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Light,
+        fontSize = 14.sp,
+        lineHeight = 22.sp,
+        color = KambioWhiteVariant
+    ),
+    labelLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Light,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 0.5.sp,
+        color = KambioWhiteVariant
     ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
+    bodySmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp,
+        lineHeight = 20.sp,
+        color = KambioWhiteVariant
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 22.sp,
+        color = KambioWhiteVariant
+    ),
+    bodyLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp,
+        lineHeight = 22.sp,
+        letterSpacing = 0.5.sp,
+        color = KambioWhiteVariant
+    ),
+    titleSmall = TextStyle(
+        fontFamily = Poppins,
         fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontSize = 14.sp,
+        lineHeight = 22.sp,
+        color = KambioWhiteVariant
+    ),
+    titleMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Medium,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp,
+        color = KambioWhiteVariant
+    ),
+    titleLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.Medium,
+        fontSize = 20.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.5.sp,
+        color = KambioWhiteVariant
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 14.sp,
+        lineHeight = 22.sp,
+        color = KambioWhiteVariant
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp,
+        color = KambioWhiteVariant
+    ),
+    headlineLarge = TextStyle(
+        fontFamily = Poppins,
+        fontWeight = FontWeight.SemiBold,
+        fontSize = 20.sp,
+        lineHeight = 28.sp,
+        letterSpacing = 0.5.sp,
+        color = KambioWhiteVariant
     )
-    */
 )
