4e40966f6166c006dc4ca0af92e0d21b63a26282
Raul Astete <raulastete.2215@gmail.com>
2024-12-19 14:47:20 -0500

    Set up initial navigation destinations

--- app/src/main/java/com/raulastete/kambio/MainActivity.kt
+++ app/src/main/java/com/raulastete/kambio/MainActivity.kt
@@ -5,7 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.raulastete.kambio.presentation.authentication.login.LoginScreen
+import com.raulastete.kambio.presentation.authentication.recover_password.RecoveryPasswordScreen
+import com.raulastete.kambio.presentation.authentication.register.RegisterScreen
+import com.raulastete.kambio.presentation.navigation.Destination
 import com.raulastete.kambio.ui.theme.KambioTheme
 
 class MainActivity : ComponentActivity() {
@@ -13,13 +19,49 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         installSplashScreen().apply {
             setKeepOnScreenCondition {
-               false
+                false
             }
         }
         enableEdgeToEdge()
         setContent {
             KambioTheme {
-                LoginScreen()
+                val navController = rememberNavController()
+
+                NavHost(
+                    navController = navController,
+                    startDestination = Destination.Login
+                ) {
+
+                    composable<Destination.Login> {
+                        LoginScreen(
+                            navigateToRecoverPassword = {
+                                navController.navigate(Destination.RecoverPassword)
+                            },
+                            navigateToRegister = {
+                                navController.navigate(Destination.Registration)
+                            }
+                        )
+                    }
+
+                    composable<Destination.Registration> {
+                        RegisterScreen(
+                            navigateBack = {
+                                navController.popBackStack()
+                            },
+                            navigateToLogin = {
+                                navController.popBackStack()
+                            }
+                        )
+                    }
+
+                    composable<Destination.RecoverPassword> {
+                        RecoveryPasswordScreen(
+                            navigateBack = {
+                                navController.popBackStack()
+                            }
+                        )
+                    }
+                }
             }
         }
     }
---
+++ app/src/main/java/com/raulastete/kambio/presentation/navigation/Destination.kt
@@ -0,0 +1,13 @@
+package com.raulastete.kambio.presentation.navigation
+
+import kotlinx.serialization.Serializable
+
+sealed interface Destination {
+
+    @Serializable
+    data object Login : Destination
+    @Serializable
+    data object Registration : Destination
+    @Serializable
+    data object RecoverPassword : Destination
+}
