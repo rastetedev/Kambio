c310e6dfe422d3a8f81d223a911d9bcc5aba6db4
Raul Emilio Astete Acuña <raulastete.2215@gmail.com>
2025-01-27 09:09:41 -0500

    Simplify CurrencyAmountInput

--- app/src/main/java/com/raulastete/kambio/presentation/home/components/CurrencyAmountInput.kt
+++ app/src/main/java/com/raulastete/kambio/presentation/home/components/CurrencyAmountInput.kt
@@ -1,5 +1,6 @@
 package com.raulastete.kambio.presentation.home.components
 
+import android.icu.text.DecimalFormat
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -13,32 +14,40 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.LocalTextStyle
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.runtime.*
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TransformedText
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.unit.dp
 import com.raulastete.kambio.R
 import com.raulastete.kambio.domain.entity.Currency
-import com.raulastete.kambio.domain.value.CurrencyAmount
-import com.raulastete.kambio.presentation.components.KambioCurrencyTextField
+import kotlin.math.max
 
 @Composable
 fun CurrencyAmountInput(
     modifier: Modifier = Modifier,
-    model: CurrencyAmountInputModel
+    model: CurrencyAmountInputModel,
+    onValueChange: (String) -> Unit
 ) {
 
-    var isFocus: Boolean by remember { mutableStateOf(false) }
+    val visualTransformation = remember {
+        CurrencyAmountInputVisualTransformation()
+    }
 
     Row(
         modifier = modifier
@@ -67,13 +76,23 @@ fun CurrencyAmountInput(
                 horizontalArrangement = Arrangement.spacedBy(4.dp)
             ) {
                 Text(
-                    text = model.currencyAmount.currency.symbol,
+                    text = model.currency.symbol,
                     style = MaterialTheme.typography.titleLarge,
                     color = MaterialTheme.colorScheme.onBackground
                 )
 
-                KambioCurrencyTextField(
-                    onFocusChanged = { isFocus = it },
+                BasicTextField(
+                    value = model.amount,
+                    onValueChange = onValueChange,
+                    textStyle = LocalTextStyle.current.copy(
+                        color = MaterialTheme.colorScheme.onBackground
+                    ),
+                    keyboardOptions = KeyboardOptions(
+                        keyboardType = KeyboardType.Decimal
+                    ),
+                    cursorBrush = SolidColor(MaterialTheme.colorScheme.primary),
+                    singleLine = true,
+                    visualTransformation = visualTransformation
                 )
             }
         }
@@ -107,19 +126,128 @@ fun CurrencyAmountInput(
 }
 
 data class CurrencyAmountInputModel(
-    val currencyAmount: CurrencyAmount,
+    val currency: Currency,
+    val amount: String,
     val title: String
 
 ) {
     val currencyLabel: String
-        @Composable get() = when (currencyAmount.currency) {
+        @Composable get() = when (currency) {
             Currency.Dollar -> stringResource(R.string.dollar_label)
             Currency.PeruvianSol -> stringResource(R.string.peruvian_sol_label)
         }
 
     val currencyIcon: Painter
-        @Composable get() = when (currencyAmount.currency) {
+        @Composable get() = when (currency) {
             Currency.Dollar -> painterResource(R.drawable.eeuu_icon)
             Currency.PeruvianSol -> painterResource(R.drawable.peru_icon)
         }
+}
+
+class CurrencyAmountInputVisualTransformation(
+    private val fixedCursorAtTheEnd: Boolean = true,
+    private val numberOfDecimals: Int = 2
+) : VisualTransformation {
+
+    private val symbols = DecimalFormat().decimalFormatSymbols
+
+    override fun filter(text: AnnotatedString): TransformedText {
+        val thousandsSeparator = symbols.groupingSeparator
+        val decimalSeparator = symbols.decimalSeparator
+        val zero = symbols.zeroDigit
+
+        val inputText = text.text
+
+        val intPart = inputText
+            .dropLast(numberOfDecimals)
+            .reversed()
+            .chunked(3)
+            .joinToString(thousandsSeparator.toString())
+            .reversed()
+            .ifEmpty {
+                zero.toString()
+            }
+
+        val fractionPart = inputText.takeLast(numberOfDecimals).let {
+            if (it.length != numberOfDecimals) {
+                List(numberOfDecimals - it.length) {
+                    zero
+                }.joinToString("") + it
+            } else {
+                it
+            }
+        }
+
+        val formattedNumber = intPart + decimalSeparator + fractionPart
+
+        val newText = AnnotatedString(
+            text = formattedNumber,
+            spanStyles = text.spanStyles,
+            paragraphStyles = text.paragraphStyles
+        )
+
+        val offsetMapping = if (fixedCursorAtTheEnd) {
+            FixedCursorOffsetMapping(
+                contentLength = inputText.length,
+                formattedContentLength = formattedNumber.length
+            )
+        } else {
+            MovableCursorOffsetMapping(
+                unmaskedText = text.toString(),
+                maskedText = newText.toString(),
+                decimalDigits = numberOfDecimals
+            )
+        }
+
+        return TransformedText(newText, offsetMapping)
+    }
+
+    private class FixedCursorOffsetMapping(
+        private val contentLength: Int,
+        private val formattedContentLength: Int,
+    ) : OffsetMapping {
+        override fun originalToTransformed(offset: Int): Int = formattedContentLength
+        override fun transformedToOriginal(offset: Int): Int = contentLength
+    }
+
+    private class MovableCursorOffsetMapping(
+        private val unmaskedText: String,
+        private val maskedText: String,
+        private val decimalDigits: Int
+    ) : OffsetMapping {
+        override fun originalToTransformed(offset: Int): Int =
+            when {
+                unmaskedText.length <= decimalDigits -> {
+                    maskedText.length - (unmaskedText.length - offset)
+                }
+
+                else -> {
+                    offset + offsetMaskCount(offset, maskedText)
+                }
+            }
+
+        override fun transformedToOriginal(offset: Int): Int =
+            when {
+                unmaskedText.length <= decimalDigits -> {
+                    max(unmaskedText.length - (maskedText.length - offset), 0)
+                }
+
+                else -> {
+                    offset - maskedText.take(offset).count { !it.isDigit() }
+                }
+            }
+
+        private fun offsetMaskCount(offset: Int, maskedText: String): Int {
+            var maskOffsetCount = 0
+            var dataCount = 0
+            for (maskChar in maskedText) {
+                if (!maskChar.isDigit()) {
+                    maskOffsetCount++
+                } else if (++dataCount > offset) {
+                    break
+                }
+            }
+            return maskOffsetCount
+        }
+    }
 }
